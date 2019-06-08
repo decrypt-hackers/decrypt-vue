30fab9d51aa0961ae33183c0828b0bce0129b4bb
myusername <avondeta3.14@gmail.com>
2019-06-08 14:32:06 +0900

    MongoDB Connection

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const consola = require('consola')
+const mongoose = require('mongoose')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
 
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
 const queuedPostsRouter = require('./routers/queuedPostsRouter')
+
+// Connect to Mongo
+mongoose.connect('mongodb+srv://' + db_user + ':' 
+    + db_pass + '@cluster0-c14lf.gcp.mongodb.net/test?retryWrites=true')
+.then(() => {
+    console.log('Connected to Server')
+})
+.catch((err) => console.log(err));
 
 async function start() {
   // Init Nuxt.js
@@ -26,7 +37,7 @@ async function start() {
   // Give nuxt middleware to express
   app.use(nuxt.render)
 
-  app.use(queuedPostsRouter)
+  app.use('queuedposts', queuedPostsRouter)
 
   // Listen the server
   app.listen(port, host)
---
+++ frontend/server/routers/queuedPostsRouter.js
@@ -0,0 +1,59 @@
+let express = require('express')
+let  bodyParser = require('body-parser')
+let queuedPosts = require('../models/queuedPost')
+
+const queuedPostsRouter = express.Router()
+
+queuedPostsRouter.use(bodyParser.json())
+
+queuedPostsRouter.route('/')
+.get((req, res, next) => {
+    queuedPosts.find({})
+    .then((queuedPost) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(queuedPost)
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+.post((req, res, next) => {
+    queuedPosts.create(req.body)
+    .then((queuedPost) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(queuedPost)
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+.delete((req, res, next) => {
+    queuedPosts.remove({})
+    .then((queuedPost) => {
+        console.log('All Lunches Deleted')
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(queuedPost)
+    }, (err) => next(err))
+    .catch((err) => next(err))
+});
+
+queuedPostsRouter.route('/:id')
+.get((req, res, next) => {
+    queuedPosts.findById(req.params.id)
+    .then((queuedPost) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(queuedPost)
+    }, (err) => next(err))
+    .catch((err) => next(err))
+})
+.delete((req, res, next) => {
+    queuedPosts.findByIdAndDelete(req.params.id)
+    .then((queuedPost) => {
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.json(queuedPost);
+    }, (err) => next(err))
+    .catch((err) => next(err))
+});
+
+module.exports = queuedPostsRouter
