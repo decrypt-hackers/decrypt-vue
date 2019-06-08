4c0928c43da77503bcecbc9066ac77b9e16aa11b
Cactice <14835424+Cactice@users.noreply.github.com>
2019-06-08 19:06:37 +0900

    Add header to index

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,22 +1,23 @@
 <template>
+    <div>
+  <Header/>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        decrypt-vue
-      </h1>
       <ArticleList></ArticleList>
     </div>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   </div>
+  </div>
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
+import Header from '~/components/Header.vue'
 import ArticleList from '../components/articleList'
 export default {
   components: {
+    Header,
     Logo,
     ArticleList
   }
