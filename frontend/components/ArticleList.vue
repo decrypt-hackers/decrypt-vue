29d2530a7edce95e25f934d2fe278e5d25a48d08
Cactice <14835424+Cactice@users.noreply.github.com>
2019-06-09 10:16:49 +0900

    Add profile pic to article list

--- frontend/components/ArticleList.vue
+++ frontend/components/ArticleList.vue
@@ -2,30 +2,46 @@
   <section class="container">
     <ul>
       <li v-for="article in articles" class="list-group-item">
-        <figure class="image is-128x128">
-          <img class="is-square avatar" src="~/assets/random/1.png">
-          <div style="color:grey;">
-          @{{ article.author }}
-          </div>
-        </figure>
-        <br />
-        <h1>{{ article.post }}</h1>
-
-        <progress class="progress" :value="article.upvotes" :max="article.upvotes+article.downvotes">15%</progress>
-        <button>
-          <font-awesome-icon
-            icon="thumbs-down"
-            style="font-size: 25px; color: hsl(204, 86%, 53%);"
-          />
-        </button>
-                        {{ article.upvotes }}
-        <button>
-          <font-awesome-icon
-            icon="thumbs-up"
-            style="font-size: 25px; color: hsl(204, 86%, 53%);"
-          />
-        </button>
+        <article class="media">
+          <figure class="media-left">
+            <figure class="image is-128x128">
+              <center>
+                <img class="is-square avatar" src="~/assets/random/3.jpg" />
+                <div style="color:grey;">@{{ article.author }}</div>
+              </center>
+            </figure>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              {{ article.post }}
+            </div>
+            <nav class="level is-mobile">
+              <div class="level-right" />
+              <div class="level-left">
+                <button>
+                  <font-awesome-icon
+                    icon="thumbs-down"
+                    style="font-size: 25px; color: hsl(204, 86%, 53%);"
+                  />
+                </button>
+                {{ article.upvotes }}
+                <button>
+                  <font-awesome-icon
+                    icon="thumbs-up"
+                    style="font-size: 25px; color: hsl(204, 86%, 53%);"
+                  />
+                </button>
                 {{ article.downvotes }}
+              </div>
+            </nav>
+            <progress
+              class="progress"
+              :value="article.upvotes"
+              :max="article.upvotes + article.downvotes"
+              >15%</progress
+            >
+          </div>
+        </article>
       </li>
     </ul>
     <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet" />
@@ -45,7 +61,7 @@ export default {
           author: 'John Doe',
           post: 'John DoeJohn DoeJohn DoeJohn Doe',
           upvotes: 3,
-          downvotes: 9,
+          downvotes: 9
         },
         {
           title: 'test 2',
@@ -53,7 +69,7 @@ export default {
           author: 'Bob',
           post: 'John DoeJohn DoeJohn DoeJohn Doe',
           upvotes: 3,
-          downvotes: 9,
+          downvotes: 9
         }
       ]
     }
